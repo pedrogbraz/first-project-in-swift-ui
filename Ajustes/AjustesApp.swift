2b6b7802eb7b28cb5b23e9d62fb836b6cd8d0164
pedrogbraz <pedrobrazdevs@gmail.com>
2024-07-26 15:47:23 -0700

    Initial Commit

---
+++ Ajustes/AjustesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AjustesApp.swift
+//  Ajustes
+//
+//  Created by Pedro Gontijo Braz on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AjustesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
